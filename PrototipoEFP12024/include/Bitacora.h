6109adca37e888af37c746f541e54e7d783ef0f5
Pablo Palencia <ppalenciar1@miumg.edu.gt>
2024-05-29 10:11:43 -0600

    Implementando bitacora

--- PrototipoEFP12024/include/Bitacora.h
+++ PrototipoEFP12024/include/Bitacora.h
@@ -1,16 +1,27 @@
 #ifndef BITACORA_H
 #define BITACORA_H
+#include <iostream>
+#include<fstream>
+#include<stdlib.h>
+#include<cstdlib>
+#include<conio.h>
+#include<iomanip>
+#include <ctime>
 
+using namespace std;
 
 class Bitacora
 {
     public:
-        Bitacora();
-        virtual ~Bitacora();
+        void ingresoDatosBitacora(string nombreUsuario, string numeAccion, string accionRealizada);
+        void imprimirDatosBitacora();
+
 
     protected:
 
     private:
+        string nombreUsuario, numeAccion, accionRealizada;
+        char* fechaHoraAccion;
 };
 
 #endif // BITACORA_H
--- PrototipoEFP12024/src/Bitacora.cpp
+++ PrototipoEFP12024/src/Bitacora.cpp
@@ -1,11 +1,35 @@
 #include "Bitacora.h"
 
-Bitacora::Bitacora()
+void Bitacora::ingresoDatosBitacora(string nombreUsuario, string numeAccion, string accionRealizada)
 {
-    //ctor
+    system("cls");
+	fstream file;
+	file.open("Bitacora.txt", ios::app | ios::out | ios::binary);
+    time_t now = time(0);
+    fechaHoraAccion = ctime(&now);
+	file <<std::left<<std::setw(15)<< nombreUsuario <<std::left<<std::setw(15)<< numeAccion <<std::left<<std::setw(15)<< accionRealizada <<std::left<<std::setw(15)<< fechaHoraAccion << "\n";
+	file.close();
 }
 
-Bitacora::~Bitacora()
-{
-    //dtor
+
+void Bitacora::imprimirDatosBitacora(){
+    system("cls");
+    fstream file;
+    string texto;
+	int total=0;
+	cout<<"\n------------------------- Imprimir Informe de Bitacora -------------------------"<<endl;
+	file.open("Bitacora.txt",ios::in);
+    while(!file.eof())
+    {
+        total++;
+        getline(file,texto);
+        cout<<texto<<endl;
+    }
+    if(total==0)
+    {
+        cout<<"\n\t\t\tNo hay informacion...";
+    }
+    file.close();
+	getch();
+	system("pause");
 }
